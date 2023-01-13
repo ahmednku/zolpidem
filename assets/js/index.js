3592e6c18fa7edf0a8eaf17a3d321a7cede933f9
ahmednku <ahmed.hameed@nkutechnologies.com>
2023-01-13 11:09:53 +0500

    reviews swiper slider fixed responsiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,13 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
   spaceBetween: 30,
+  slidesPerView: "auto",
   centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    dynamicBullets: true,
   },
 });
